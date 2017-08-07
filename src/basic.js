5c758da0a02b69968b94966c6ac8677aa68b8dac
Yunfen <yfqiu@thoughtworks.com>
2017-08-07 10:51:34 +0800

    typescript function

--- src/basic.js
+++ src/basic.js
@@ -13,7 +13,7 @@ var fibonacci1 = [1, 2, 3, 5, 8];
 var numArray = [1, 2, 3, 5, 8];
 //数组中出现任意类型
 var list = ['Xcat Liu', 25, { website: 'http://xcatliu.com' }];
-//函数声明
+//函数声明，严格匹配参数列表（个数与类型）
 function sum(x, y) {
     return x + y;
 }
@@ -21,3 +21,43 @@ function sum(x, y) {
 var mySum = function (x, y) {
     return x + y;
 };
+var mySearch;
+mySearch = function (source, subString) {
+    return source.search(subString) !== -1;
+};
+//函数中包含可选参数
+function buildName(firstName, lastName) {
+    if (lastName) {
+        return firstName + ' ' + lastName;
+    }
+    return firstName;
+}
+var xcatliu = buildName('Xcat', 'Liu');
+var xcat = buildName('Xcat');
+//函数参数添加默认值，TypeScript会将添加了默认值的参数识别为可选参数，并不受「可选参数必须接在必需参数后面」的限制
+function buildNameDefaultValue(firstName, lastName) {
+    if (lastName === void 0) { lastName = 'Qiu'; }
+    return firstName + lastName;
+}
+var xcatliu1 = buildNameDefaultValue('Xcat', 'Liu');
+var xcat1 = buildNameDefaultValue('Xcat');
+//剩余参数
+function push(array) {
+    var items = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        items[_i - 1] = arguments[_i];
+    }
+    items.forEach(function (item) {
+        array.push(item);
+    });
+}
+var a = [];
+push(a, 1, 2, 3);
+function reverse(x) {
+    if (typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    }
+    else {
+        return x.split('').reverse().join('');
+    }
+}
--- src/basic.ts
+++ src/basic.ts
@@ -22,7 +22,7 @@ let numArray: numberArray = [1,2,3,5,8];
 //数组中出现任意类型
 let list: any[] = ['Xcat Liu', 25, { website: 'http://xcatliu.com' }];
 
-//函数声明
+//函数声明，严格匹配参数列表（个数与类型）
 function sum(x: number, y: number) : number {
     return x + y;
 }
@@ -30,3 +30,52 @@ function sum(x: number, y: number) : number {
 let mySum:(x: number, y: number) => number = function(x: number, y: number):number {
     return x + y;
 }
+
+//用接口定义函数形状
+interface searchFun{
+    (source: string, subString: string) : boolean;
+}
+
+let mySearch : searchFun;
+mySearch = function(source: string, subString: string) : boolean {
+    return source.search(subString) !== -1;
+}
+
+//函数中包含可选参数
+function buildName(firstName: string, lastName?: string) {
+    if(lastName) {
+        return firstName + ' ' + lastName;
+    }
+    return firstName;
+}
+let xcatliu = buildName('Xcat', 'Liu');
+let xcat = buildName('Xcat');
+
+//函数参数添加默认值，TypeScript会将添加了默认值的参数识别为可选参数，并不受「可选参数必须接在必需参数后面」的限制
+function buildNameDefaultValue(firstName: string, lastName: string = 'Qiu') {
+    return firstName + lastName;
+}
+
+let xcatliu1 = buildNameDefaultValue('Xcat', 'Liu');
+let xcat1 = buildNameDefaultValue('Xcat');
+
+//剩余参数
+function push(array: any[], ...items: any[]) {
+  items.forEach(function(item) {
+    array.push(item);
+  });
+}
+let a = [];
+push(a, 1, 2, 3);
+
+//函数重载，允许一个函数接受不同数量或类型的参数时，作出不同的处理
+function reverse(x: number) : number;
+function reverse(x: string) : string;
+function reverse(x: number | string) : number | string {
+    if(typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    } else {
+        return x.split('').reverse().join('');
+    }
+}
+
